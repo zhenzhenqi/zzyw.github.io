e6ff631e267ad6bcdd15058b64c3e63e2c975d3d
Yang Wang <yang.wang.dec@gmail.com>
2019-11-21 12:25:22 -0500

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,26 @@
-let cat1 = document.getElementById("");
-// let cat2 = 
+---
+---
+
+
+let postTitle = $("#content").attr('post-title');
+let postCategory = $("#content").attr('post-category');
+
+let nav1Items = $("#nav1 a");
+let nav2Items = $('#nav2 a');
+
+
+//highlight nav 1 selected item
+nav1Items.each(function(index, value) {
+  let attr = value.getAttribute("link-cat");
+
+  if (attr === postCategory) {
+    $(value).addClass("selected");
+  }
+});
+
+nav2Items.each(function(index, value) {
+  let attr = value.getAttribute("link-cat");
+  if (attr === postTitle) {
+    $(value).addClass("selected");
+  }
+})
